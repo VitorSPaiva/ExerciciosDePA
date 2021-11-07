b59d017be9beda1153cf936cc18df86f98cc285c
VitorSPaiva <vitorsantospaiva@hotmail.com>
2021-11-07 17:57:47 -0300

    enviado

---
+++ questao6/Resposta.c
@@ -0,0 +1,11 @@
+/*
+*	6. Assumindo que pulo[] é um vetor do tipo int, quais das seguintes expressões referenciam o
+*	valor do terceiro elemento do vetor?
+*/
+
+*(pulo + 2); // conteúdo de pulo[3]
+*(pulo + 4); // conteúdo de pulo[5]
+pulo + 4; // endereço do pulo[5]
+pulo + 2; // endereço do pulo[3]
+
+// A expressão que referencia o valor do terceiro elemento do vetor é *(pulo + 2);
